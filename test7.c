59272a7783bdd3bc423d9b9ea27c57ba489ba6a2
Manjesh30Verma <30manjeshwarverma0002@gmail.com>
2023-09-22 11:23:22 +0530

    assignment-1 programs

---
+++ test7.c
@@ -0,0 +1,10 @@
+//Write a program in C to check whether the number is prime or not? take input from user use if else
+
+#include<stdio.h> // This is predefined function library means predefined funtion like printf()
+void main() // This is main function
+{
+    int num; // The given number is taken in a integer type of varible
+    int add; // This is a add variable to store the value of resultant variable
+    add=num1+num2; // In which the addition two numbers over here
+    printf("Addition of two numbers is: %d",add); // We print this message to output screen
+}
