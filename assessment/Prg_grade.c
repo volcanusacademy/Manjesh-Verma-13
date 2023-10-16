f1c7ecc968f1b52cc7a3d70c4e5719bc896a33a5
Manjesh Verma <144987266+Manjesh30Verma@users.noreply.github.com>
2023-10-16 18:44:43 +0530

    Prg_grade.c

--- assessment/Prg_grade.c
+++ assessment/Prg_grade.c
@@ -1,42 +1,44 @@
-#include <stdio.h>
-void main()
-{ 
-    
-	float Percentage;// defined a decimal variable and intialized with zero otherwise it will collect garbage value
+#include<stdio.h> // This is the input output library of predefined function like printf or scanf
+
+void main() // main function the whole function call the code first
+{
+    float Percentage=0;// defined a decimal variable and intialized with zero otherwise it will collect garbage value
  
-    printf("Please Enter the Percentage : \n");
+    printf("Enter Percentage: ");
     
-	// Take a integer numbers from user as a input / student marks
-	scanf("%f", &Percentage);
- 
+    // Take a Percentage from user as a input
+    scanf("%f", &Percentage);
+    
+    
+    
     // 'if'-- conditional statement will check the condition and execute the functionality
-	if ( Percentage >= 90)
+    if(Percentage >= 90)
     {
-    	printf( " \n Grade A");
-	}
-	
-	 
-    // 'else if' also check the condition and execute the functionality
-	else if ( Percentage >= 80)
+        printf("Grade = A++");
+    }
+    
+    
+    // 'else if' also check the both condition and execute the functionality
+    else if(Percentage >= 80 && Percentage < 90)
     {
-    	printf("\n Grade B");
-	}
-	else if ( Percentage >= 70 )
+        printf("Grade = A");
+    }
+    else if(Percentage >= 70 && Percentage < 80)
     {
-    	printf(" \n Grade C");
-	}
-	else if ( Percentage >= 60)
+        printf("Grade = B");
+    }
+    else if(Percentage >= 60 && Percentage < 70)
     {
-    	printf("\n Grade D");
-	}
-	else if ( Percentage >= 40)
+        printf("Grade = C");
+    }
+    else if(Percentage >= 50 && Percentage < 60)
     {
-    	printf("\n Grade E");
-	}
-	
-	//condition are false execute the 'else' part
-	else 
+        printf("Grade = D");
+    }
+    //condition are false execute the 'else' part
+    else 
     {
-    	printf("\n Fail");
-	} 
+        printf("Fail");
+    }
+                    
 }
